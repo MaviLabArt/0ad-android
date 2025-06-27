TaoJiaHengOne/0ad

commit fd847c2a23814677a5ac3c3e95920ea1cc90fe27

Refactor GUI object resizing JS code passages
It is possible to modify sizes by setting the properties directly.
(Without having to create a copy of it)
This allows to shorten those passages quite a bit without any functional change.

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/Panels/Chat/ChatInputPanel.js
@@ -8,9 +8,7 @@ class ChatInputPanel
 		resizeGUIObjectToCaption(this.chatSubmitButton, { "horizontal": "left" }, { "horizontal": 8 });
 
 		this.chatInput = Engine.GetGUIObjectByName("chatInput");
-		const size = this.chatInput.size;
-		size.right = this.chatSubmitButton.size.left;
-		this.chatInput.size = size;
+		this.chatSubmitButton.size.right = this.chatSubmitButton.size.left;
 
 		this.chatInput = Engine.GetGUIObjectByName("chatInput");
 		this.chatInput.tooltip = colorizeAutocompleteHotkey(this.Tooltip);

MaviLabArt/0ad-android

commit 3db465587fa11383b3f3c316b222069427566056

Fix chat input size assignment typo
Fixes a typo introduced in fd847c2a23 where the code
incorrectly modified chatSubmitButton.size instead of
chatInput.size, effectively collapsing the input field's
width to zero.

This change ensures the chat input field resizes correctly
based on the button's position.

Thanks to Langbarg for noticing the issue.

Refs: #8138

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/Panels/Chat/ChatInputPanel.js
@@ -8,7 +8,7 @@ class ChatInputPanel
 		resizeGUIObjectToCaption(this.chatSubmitButton, { "horizontal": "left" }, { "horizontal": 8 });
 
 		this.chatInput = Engine.GetGUIObjectByName("chatInput");
-		this.chatSubmitButton.size.right = this.chatSubmitButton.size.left;
+		this.chatInput.size.right = this.chatSubmitButton.size.left;
 
 		this.chatInput = Engine.GetGUIObjectByName("chatInput");
 		this.chatInput.tooltip = colorizeAutocompleteHotkey(this.Tooltip);