MaviLabArt/0ad-android

commit 5e4b49f65fde55924a552781e45aba42470b30b4

Fix some includes in source/renderer
Make include-what-you-use happy with some files in source/renderer and
fix what needs to be fixed.

Ref: #8086
Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

source/renderer/backend/IDevice.h
@@ -19,24 +19,36 @@
 #define INCLUDED_RENDERER_BACKEND_IDEVICE
 
 #include "graphics/Color.h"
-#include "ps/containers/Span.h"
-#include "renderer/backend/Backend.h"
-#include "renderer/backend/Format.h"
 #include "renderer/backend/IBuffer.h"
 #include "renderer/backend/IDevice.h"
-#include "renderer/backend/IDeviceCommandContext.h"
-#include "renderer/backend/IFramebuffer.h"
-#include "renderer/backend/IShaderProgram.h"
 #include "renderer/backend/ITexture.h"
-#include "renderer/backend/PipelineState.h"
-#include "scriptinterface/ScriptForward.h"
 
+#include <cstdint>
+#include <js/TypeDecls.h>
 #include <memory>
 #include <string>
 #include <vector>
 
 class CShaderDefines;
 class CStr;
+class ScriptRequest;
+namespace PS { template <typename T> class span; }
+namespace Renderer::Backend { class IComputePipelineState; }
+namespace Renderer::Backend { class IDeviceCommandContext; }
+namespace Renderer::Backend { class IFramebuffer; }
+namespace Renderer::Backend { class IGraphicsPipelineState; }
+namespace Renderer::Backend { class IShaderProgram; }
+namespace Renderer::Backend { class IVertexInputLayout; }
+namespace Renderer::Backend { enum class AttachmentLoadOp; }
+namespace Renderer::Backend { enum class AttachmentStoreOp; }
+namespace Renderer::Backend { enum class Backend; }
+namespace Renderer::Backend { enum class Format; }
+namespace Renderer::Backend { struct SColorAttachment; }
+namespace Renderer::Backend { struct SComputePipelineStateDesc; }
+namespace Renderer::Backend { struct SDepthStencilAttachment; }
+namespace Renderer::Backend { struct SGraphicsPipelineStateDesc; }
+namespace Renderer::Backend { struct SVertexAttributeFormat; }
+namespace Renderer::Backend::Sampler { struct Desc; }
 
 namespace Renderer
 {

commit 8a8f2596ea9666577fe78ac67d6b9176aff52460

Fix a warning on Windows
5e4b49f65f introduced a warning, silence it as it's in spidermonkey and
only leaks while on vs2017.

Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

source/renderer/backend/IDevice.h
@@ -24,7 +24,15 @@
 #include "renderer/backend/ITexture.h"
 
 #include <cstdint>
+#if MSC_VERSION
+# pragma warning(push, 1)
+# pragma warning(disable: 4068)
+# pragma warning(disable: 4100)
+#endif
 #include <js/TypeDecls.h>
+#if MSC_VERSION
+# pragma warning(pop)
+#endif
 #include <memory>
 #include <string>
 #include <vector>