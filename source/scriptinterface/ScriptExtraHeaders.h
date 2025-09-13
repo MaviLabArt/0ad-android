c4762e22ccda8cda20c061b6c08895877d1f9e7d
Itms <itms@wildfiregames.com>
2025-09-13 12:12:11 +0200

    Remove obsolete `signbit` GCC workaround
    Reviewed-At: https://gitea.wildfiregames.com/0ad/0ad/pulls/8324

--- source/scriptinterface/ScriptExtraHeaders.h
+++ source/scriptinterface/ScriptExtraHeaders.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2023 Wildfire Games.
+/* Copyright (C) 2025 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -23,11 +23,6 @@
 // just include ScriptTypes.h directly to get the standard jsapi.h.)
 
 #include "scriptinterface/ScriptTypes.h"
-
-// Redefine signbit to fix build error in GCC
-#ifndef signbit
-# define signbit std::signbit
-#endif
 
 #include "jsfriendapi.h"
 
